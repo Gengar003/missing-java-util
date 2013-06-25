a373a9546cac30be47089604f5c6298cee77b5e5
Austin Witt <awitt@homeaway.com>
2013-06-25 09:51:29 -0500

    Updated: - converted to maven project in Eclipse. - ignoring .settings folder

--- src/test/java/org/gengar/util/collection/TestCollectionMap.java
+++ src/test/java/org/gengar/util/collection/TestCollectionMap.java
@@ -15,9 +15,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.gengar.util.collection;
 
-import org.junit.Test;
-
 import java.util.List;
+
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -30,16 +30,6 @@ public class TestCollectionMap {
 
 	@Test
 	public void basic_use_case() {
-
-		IMapOfLists<String,Integer> simple_map = new GeneralListMap<>();
-		simple_map.put( "cat", 5 );
-		simple_map.put( "cat", 6 );
-		simple_map.put("dog", 10);
-		simple_map.put( "cat", 5 );
-
-		List<Integer> cats = simple_map.get( "cat" );
-		Integer specific_cat = simple_map.get( "cat", 0 );
-
-		assertEquals(specific_cat, (Integer)5);
+		
 	}
 }
